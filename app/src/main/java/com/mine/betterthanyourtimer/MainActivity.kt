80365c7e86afb48cdee950c95990c9a09cc5b8b8
“Esther <esthcarelle@email.com>
2023-05-12 15:22:23 +0200

    add the timer screen composable to the main screen

--- app/src/main/java/com/mine/betterthanyourtimer/MainActivity.kt
+++ app/src/main/java/com/mine/betterthanyourtimer/MainActivity.kt
@@ -1,35 +1,13 @@
 package com.mine.betterthanyourtimer
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.mine.betterthanyourtimer.components.TimerScreen
 import com.mine.betterthanyourtimer.ui.theme.BetterThanYourTimerTheme
-import com.mine.betterthanyourtimer.ui.theme.bgDarkBlue
-import com.mine.betterthanyourtimer.ui.theme.purple
 import com.mine.betterthanyourtimer.viewModels.TimerViewModel
 
 class MainActivity : ComponentActivity() {
@@ -48,6 +26,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 
 
